be8fe87e87e7a673873c5f6b4c3fc5c38b870f35
apodeme <alexcoulton@gmail.com>
2019-08-21 11:17:13 +0100

    Update faoneline.sh
    Added automatic file handling / directory handling

--- faoneline.sh
+++ faoneline.sh
@@ -4,27 +4,35 @@
 #requires folders oneline/wolineone
 
 #convert all files in txt from multiline fasta to single line
+mkdir ./oneline
+
 count=1
-numfiles=$(($(wc -l ./taxafilenames.txt | awk '{print $1}') + 1)) 
+numfiles=$(($(ls -l ./*.fa | wc -l | awk '{print $1}') - 1)) 
 echo $numfiles
 while [ $count -le $numfiles ]; do
-	curfile=$(sed "${count}q;d" ./taxafilenames.txt)
+	curfile=$(ls -l | awk '{print $9}' | sed "${count}q;d")
 	curfile=$(echo "$curfile")
+	echo $curfile
 	awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < ./$curfile > ./oneline/$curfile
 	count=$(($count + 1))
 done
 
 
-
+mkdir ./oneline/wolineone
 #remove first blank line produced by awk with sed
 count=1
-numfiles=$(($(wc -l ./taxafilenames.txt | awk '{print $1}') + 1)) 
+numfiles=$(($(ls -l ./*.fa | wc -l | awk '{print $1}') - 1))  
 echo $numfiles
 while [ $count -le $numfiles ]; do
-	curfile=$(sed "${count}q;d" ./taxafilenames.txt)
-	curfile2=$(sed "${count}q;d" ./taxafilenames.txt)
+	curfile=$(ls -l | awk '{print $9}' | sed "${count}q;d")
+	curfile2=$(ls -l | awk '{print $9}' | sed "${count}q;d")	
 	curfile=$(echo "$curfile")
 	sed 1d ./oneline/$curfile > ./oneline/wolineone/$curfile2
 	count=$(($count + 1))
 done
 
+mkdir multi.fa
+mv ./*.fa multi.fa
+
+mv ./oneline/wolineone/*.fa ./
+rm -r ./oneline
